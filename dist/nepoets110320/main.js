ffe22f05259455b5383240a612c89f9ade085ee0
ksadeski2019 <ksadeski@uga.edu>
2020-11-07 14:37:27 -0500

    updated build

--- dist/nepoets110320/main.js
+++ dist/nepoets110320/main.js
@@ -1096,7 +1096,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_3__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ksade\Desktop\nepoets110320\nepoets110320\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\ksade\Desktop\nepoets\src\main.ts */"./src/main.ts");
 
 
 /***/ })
